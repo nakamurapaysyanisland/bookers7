6f7d9987ebb37dda37a1998399f01ec08308c07c
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-01-09 10:23:30 +0000

    [add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_paramerters, if: :devise_controller?
+
+    
+    protected
+
+    def configure_permitted_paramerters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    private
+    def after_sign_in_path_for(resource)
+        user_path(resource)
+    end
+    def after_sign_out_path_for(resource)
+        root_path(resource)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
+  
 end
