5ee80f7d46b2c3ddba50bae5a134e4abbcc71459
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-01-09 12:48:26 +0000

    [add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_paramerters, if: :devise_controller?
-
+    # deviseに関する処理（コントローラー）の時だけ、このメソッドを実行する
     
     protected
 
-    def configure_permitted_paramerters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+       devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     end
 
     private
